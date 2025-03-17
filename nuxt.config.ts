82a31f161ca4509bfd507a613d9286d082ea1e77
Kat Hartog <kathryn.hartog@gmail.com>
2025-03-17 03:03:23 -0400

    add nuxt deployment preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  nitro: {
+    preset: 'netlify-static'
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
